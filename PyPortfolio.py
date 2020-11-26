e34957d439e032d7166aab9621b1b444baf18af6
Ashrit <ashritkasshyap@icloud.com>
2020-11-26 13:35:21 +0530

    added portfolio return and portfolio vol to the class

--- PyPortfolio.py
+++ PyPortfolio.py
@@ -3,7 +3,22 @@ from RiskReturn import *
 
 class PyPortfolio(RiskReturn):
 
-    def __init__(self, return_series, periodicity, weights):
+    def __init__(self, return_series, periodicity, weights=None):
         RiskReturn.__init__(self,
                             return_series=return_series,
                             periodicity=periodicity)
+
+        self.weights = weights
+
+        if self.weights == None:
+            n = self.return_series.shape[1]
+            self.weights = np.repeat(1/n, n)
+
+        # returns the portfolio returns as a RiskReturn object
+        self.portfolio_return = RiskReturn(return_series=portfolio_returns(return_series=self.return_series,
+                                                                           weights=self.weights),
+                                           periodicity=self.periodicity,
+                                           risk_free_rates=self.RiskFreeRates)
+
+        self.portfolio_volatility = portfolio_volatility(return_series=self.return_series,
+                                                         weights=self.weights)
